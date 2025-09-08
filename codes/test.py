041e81e561dce2cde998562ad6a1be19423d8934
Sreepathy <sreepathy200567@gmail.com>
2025-09-08 07:44:26 +0530

    Test file completed

--- codes/test.py
+++ codes/test.py
@@ -1,2 +1,15 @@
 import pandas as pd
+from jobs import Job
 
+Jobs = []
+
+def test_job_creation():
+    for i in range(10):
+        machine = input("enter machine requried (example A->B->C)")
+        Jobs.append(Job(i, machine.split(" ")))
+    
+
+if __name__ == "__main__":
+    test_job_creation()
+    df = pd.DataFrame(Jobs,columns=["ID","Priority","Machines","Machine Running Time", "Total Time per Job", 
+                               "Temperature Reduction" , "Job failure rate" , "Status"])
