3a7659c97631a5923bc7f5f5981bb81415ce4b57
Sreepathy <sreepathy200567@gmail.com>
2025-09-08 07:37:33 +0530

    Minor change onto Job paramets

--- codes/jobs.py
+++ codes/jobs.py
@@ -1,12 +1,12 @@
 import random
 
 class Job:
-    def __init__(self, job_id, total_processing_time,machine_requirement,):
+    def __init__(self, job_id,machine_requirement,):
         self.job_id = job_id                      # Unique identifier for the job
         self.job_priority = 0
-        self.total_processing_time = total_processing_time  # Time required to execute the job
         self.machine_requirement = machine_requirement  # Specific machine or None
         self.machine_processing_time = [random.randint(1,5) for i in self.machine_requirement]
+        self.total_processing_time = sum(self.machine_processing_time)  # Time required to execute the job
         self.threshold_temperature_reduction = random.uniform(0.1, 0.5)
         if random.random() < 0.1:
             self.job_failure_rate = random.uniform(0.4,0.8)
