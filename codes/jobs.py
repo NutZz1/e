551cb5de879fc5dc6fb5ade49f87f80f8a487d13
Sreepathy <sreepathy200567@gmail.com>
2025-09-08 07:21:45 +0530

    updated Job

--- codes/jobs.py
+++ codes/jobs.py
@@ -1,1 +1,16 @@
-print("hello")
+import random
+
+class Job:
+    def __init__(self, job_id, total_processing_time, machine_processing_time,machine_requirement,):
+        self.job_id = job_id                      # Unique identifier for the job
+        self.total_processing_time = total_processing_time  # Time required to execute the job
+        self.machine_requirement = machine_requirement  # Specific machine or None
+        self.machine_processing_time = machine_processing_time
+        self.threshold_temperature_reduction = random.uniform(0.1, 0.5)
+        self.status = 'Not Started'             # Job status: Not Started / Running / Completed
+
+    def start(self):
+        self.status = 'Running'
+
+    def complete(self):
+        self.status = 'Completed'
